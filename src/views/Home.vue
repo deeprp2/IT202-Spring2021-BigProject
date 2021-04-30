e989b82b83ef30073d6e09e5cc9ec9edea1ee6f6
Deep Patel <43456929+deeprp2@users.noreply.github.com>
2021-04-30 15:18:31 -0500

    Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,24 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div align="left" class="px-2">
+    <h1>Stockify</h1>
+    <p>
+      A Single-Page App that allows users to search and analyze stocks.
+    </p>
+
+    <h2>Instructions</h2>
+    <p>
+      <v-list>
+        <v-list-item v-for="(instruction, key) in instructions" :key="key">
+          <v-list-item-content>
+            <v-list-item-title class="text-wrap">{{instruction}}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </p>
+
+    <v-btn class="info" href="/search">
+      Search
+    </v-btn>
   </div>
 </template>
 
@@ -9,5 +27,14 @@
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      instructions: [
+          "1. Use the search page to pick a stock",
+          "2. Navigate to charts to see charts related to the stock",
+          "3. Visit the news page to see news about a stock"
+      ]
+    }
+  }
 }
 </script>
