4439d26de36336118d40b8add50c871d37a9a110
Deep Patel <43456929+deeprp2@users.noreply.github.com>
2021-05-02 20:17:11 -0500

    adding dist subtree

---
+++ dist/precache-manifest.ac823cf2629a0ea335bc9bdc2b550a38.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b585a8d37510cbd5bf8",
+    "url": "/IT202-Spring2021-BigProject/css/about.41710611.css"
+  },
+  {
+    "revision": "c9ea2fe0e8b838d30c64",
+    "url": "/IT202-Spring2021-BigProject/css/app.44e8b7ef.css"
+  },
+  {
+    "revision": "2c35f4dfecd09c59d46a",
+    "url": "/IT202-Spring2021-BigProject/css/chunk-vendors.feb709c4.css"
+  },
+  {
+    "revision": "8a715b967df738826b4946886418071a",
+    "url": "/IT202-Spring2021-BigProject/index.html"
+  },
+  {
+    "revision": "0b585a8d37510cbd5bf8",
+    "url": "/IT202-Spring2021-BigProject/js/about.88f7c3b3.js"
+  },
+  {
+    "revision": "c9ea2fe0e8b838d30c64",
+    "url": "/IT202-Spring2021-BigProject/js/app.76da3867.js"
+  },
+  {
+    "revision": "2c35f4dfecd09c59d46a",
+    "url": "/IT202-Spring2021-BigProject/js/chunk-vendors.3317c3f6.js"
+  },
+  {
+    "revision": "9b8868def8269bcbd480922dcc3694e4",
+    "url": "/IT202-Spring2021-BigProject/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/IT202-Spring2021-BigProject/robots.txt"
+  }
+]);
