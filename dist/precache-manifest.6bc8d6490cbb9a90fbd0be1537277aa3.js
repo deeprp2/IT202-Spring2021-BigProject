1105555b71abb67d5a3158502dbb03edab02b086
Deep Patel <43456929+deeprp2@users.noreply.github.com>
2021-05-02 20:46:14 -0500

    adding dist subtree

--- dist/precache-manifest.6bc8d6490cbb9a90fbd0be1537277aa3.js
+++ dist/precache-manifest.6bc8d6490cbb9a90fbd0be1537277aa3.js
@@ -4,7 +4,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/IT202-Spring2021-BigProject/css/about.41710611.css"
   },
   {
-    "revision": "c9ea2fe0e8b838d30c64",
+    "revision": "ad93b28da71800b6912d",
     "url": "/IT202-Spring2021-BigProject/css/app.44e8b7ef.css"
   },
   {
@@ -12,7 +12,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/IT202-Spring2021-BigProject/css/chunk-vendors.feb709c4.css"
   },
   {
-    "revision": "8a715b967df738826b4946886418071a",
+    "revision": "f1806be07f9105003ede0d7381001030",
     "url": "/IT202-Spring2021-BigProject/index.html"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/IT202-Spring2021-BigProject/js/about.88f7c3b3.js"
   },
   {
-    "revision": "c9ea2fe0e8b838d30c64",
-    "url": "/IT202-Spring2021-BigProject/js/app.76da3867.js"
+    "revision": "ad93b28da71800b6912d",
+    "url": "/IT202-Spring2021-BigProject/js/app.4e4fdd99.js"
   },
   {
     "revision": "2c35f4dfecd09c59d46a",
