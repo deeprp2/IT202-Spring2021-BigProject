47148b1bb9da93887540a3376e6433b5fd2f97b8
Deep Patel <43456929+deeprp2@users.noreply.github.com>
2021-05-02 20:02:55 -0500

    api endpoint changes

--- src/views/Charts.vue
+++ src/views/Charts.vue
@@ -75,21 +75,26 @@ export default {
   },
   async mounted() {
     await this.populateStockPriceData();
-    this.chartData.datasets[0].data = this.priceData.map(item => item["volume"])
-    this.chartData.labels = this.priceData.map(item => item["date"]).map(time => time.substr(0, time.indexOf('T')));
+    this.chartData.datasets[0].data = this.priceData.map((item) => {return parseInt(item["5. volume"])})
+    this.chartData.labels = this.priceData.map(item => item["date"])
     this.lineChartData.labels = this.chartData.labels
-    this.lineChartData.datasets[0].data = this.priceData.map(item => item["high"])
-    this.lineChartData.datasets[1].data = this.priceData.map(item => item["low"])
+    this.lineChartData.datasets[0].data = this.priceData.map(item => Math.floor(parseFloat(item["2. high"])))
+    this.lineChartData.datasets[1].data = this.priceData.map(item => Math.floor(parseFloat(item["3. low"])))
     this.renderCharts = true;
   },
   methods: {
     async populateStockPriceData() {
-      await axios.get(`http://api.marketstack.com/v1/eod?access_key=${process.env.VUE_APP_EOD_API_KEY}&symbols=${this.selectedStock}&date_from=2020-05-01`)
+      await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${this.selectedStock}&outputsize=compact&apikey=${process.env.VUE_APP_EOD_API_KEY}`)
         .then(async (response) => {
-          let data = response.data;
-          data.data.forEach((stock) => {
-            this.priceData.push(stock)
-          })
+          let data = response.data["Time Series (Daily)"];
+
+          for (const [key, value] of Object.entries(data)) {
+            let obj = {
+              "date": key,
+              ...value
+            }
+            this.priceData.push(obj)
+          }
         })
     }
   }
